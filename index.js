fa705c08640b10991e93a8c8389f2c3ee56d4760
sunitee <suniteebajpai2@gmail.com>
2021-04-12 16:35:35 -0400

    commit tree 81f2c60951169cbfb7bd529f7c9d6274952bbd6f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+const sourceArray = [1, 2, 3, -9]
+
+function map(sourceArray, fn) {
+     return sourceArray.map(i => fn(i))
+ }
+
+ map([1, 2, 3, -9], function(a){ return a * a }) //=> [1, 4, 9, 81] 
+
+
+function reduce(arr, fn, start) {
+    let result = (!!start) ? start : arr[0];
+    let i = (!!start) ? 0 : 1;
+
+    for (; i < arr.length; i++) {
+        result = fn(arr[i], result);
+    }
+
+    return result;
+}
